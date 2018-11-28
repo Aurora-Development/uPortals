784730a5a2a5be28754cdfdf23625e0d753798bd
Red <36905383+RedCraftGH@users.noreply.github.com>
2018-11-28 15:51:08 -0500

    Create Portal.php

---
+++ src/RedCraftPE/uPortals/Commands/Portal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RedCraftPE\uPortals\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+
+use RedCraftPE\uPortals\Main;
+
+class Portal {
+
+}
