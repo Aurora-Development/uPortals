e5298aa74be9005f45274da10078627d78f4e107
Red <36905383+RedCraftGH@users.noreply.github.com>
2018-11-28 20:40:27 -0500

    Create Wand.php

---
+++ src/RedCraftPE/uPortals/Commands/SubCommands/Wand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RedCraftPE\uPortals\Commands\SubCommands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+
+use RedCraftPE\uPortals\Main;
+
+class Wand {
+
+	public function onWandCommand(CommandSender $sender) {
+	
+		return false;
+	}
+}
