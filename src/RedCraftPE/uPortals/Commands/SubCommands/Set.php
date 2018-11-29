ad8f2173ba82b1260eca7e1f22669a4aae893088
Red <36905383+RedCraftGH@users.noreply.github.com>
2018-11-28 20:43:33 -0500

    Create Set.php

---
+++ src/RedCraftPE/uPortals/Commands/SubCommands/Set.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RedCraftPE\uPortals\Commands\SubCommands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+
+use RedCraftPE\uPortals\Main;
+
+class Set {
+
+	public function onSetCommand(CommandSender $sender, array $args) {
+	
+		return false;
+	}
+}
