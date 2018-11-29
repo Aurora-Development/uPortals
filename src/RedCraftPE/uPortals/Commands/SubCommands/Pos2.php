6e90bad8eece42f17d7349227020e29ac5fc60d5
Red <36905383+RedCraftGH@users.noreply.github.com>
2018-11-28 20:42:55 -0500

    Create Pos2.php

---
+++ src/RedCraftPE/uPortals/Commands/SubCommands/Pos2.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RedCraftPE\uPortals\Commands\SubCommands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+
+use RedCraftPE\uPortals\Main;
+
+class Pos2 {
+
+	public function onPos2Command(CommandSender $sender) {
+	
+		return false;
+	}
+}
