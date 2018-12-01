c3b3dfcc30438c242939a8de6ba3e41d6617c820
Red <36905383+RedCraftGH@users.noreply.github.com>
2018-11-30 19:21:50 -0500

    Finished?

--- src/RedCraftPE/uPortals/Commands/SubCommands/Wand.php
+++ src/RedCraftPE/uPortals/Commands/SubCommands/Wand.php
@@ -4,6 +4,7 @@ namespace RedCraftPE\uPortals\Commands\SubCommands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
+use pocketmine\item\Item;
 
 use RedCraftPE\uPortals\Main;
 
@@ -11,6 +12,19 @@ class Wand {
 
 	public function onWandCommand(CommandSender $sender) {
 	
+		if ($sender->hasPermission("uportals.wand")) {
+			
+			$ench = Enchantment::getEnchantment(2);
+			$enchInstance = new EnchantmentInstance($ench, 1);
+			$wand = Item::get(Item::WOODEN_AXE)->setCustomName(TextFormat::BLUE . "Portal Wand")->addEnchantment($enchInstance));
+			$sender->getInventory()->addItem($wand);
+			$sender->sendMessage(TextFormat::GREEN . "The portal wand is in your inventory!");
+			return true;
+		} else {
+		
+			$sender->sendMessage(TextFormat::RED . "You do not have permissions to run this command.");
+      			return true;
+		}
 		return false;
 	}
 }
