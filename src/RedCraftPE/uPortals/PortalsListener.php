db0007d40468a4645c2e2b40edf6b4e601a6cbd7
Red <36905383+RedCraftGH@users.noreply.github.com>
2018-12-01 14:07:18 -0500

    Update PortalsListener.php

--- src/RedCraftPE/uPortals/PortalsListener.php
+++ src/RedCraftPE/uPortals/PortalsListener.php
@@ -4,6 +4,7 @@ namespace RedCraftPE\uPortals;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerInteractEvent;
 
 class PortalsListener implements Listener {
 
@@ -18,5 +19,34 @@ class PortalsListener implements Listener {
   public function onMove(PlayerMoveEvent $event) {
   
     $player = $event->getPlayer();
+  }
+  public function onInteract(PlayerInteractEvent $event) {
+    
+    $plugin = $this->plugin;
+    $player = $event->getPlayer();
+    $item = $event->getItem();
+    $action = $event->getAction();
+    $block = $event->getBlock();
+    $blockX = $block->getX();
+    $blockY = $block->getY();
+    $blockZ = $block->getZ();
+    $portalsArray = $plugin->portals->get("Portals", []);
+    
+    if ($item->getName() === TextFormat::BLUE . "Portal Wand") {
+      
+      if ($action === 1) {
+      
+        $portalsArray["pos1"] = $blockX . ", " . $blockY . ", " . $blockZ;
+        $plugin->portals->set("Portals", $portalsArray);
+        $plugin->portals->save();
+        $player->sendMessage(TextFormat::GREEN . "Portal position 1 is now {$blockX}, {$blockY}, {$blockZ}");
+      } else {
+      
+        $portalsArray["pos2"] = $blockX . ", " . $blockY . ", " . $blockZ;
+        $plugin->portals->set("Portals", $portalsArray);
+        $plugin->portals->save();
+        $player->sendMessage(TextFormat::GREEN . "Portal position 1 is now {$blockX}, {$blockY}, {$blockZ}");
+      }
+    }
   }
 }
